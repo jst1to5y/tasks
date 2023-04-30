5d93936a79abb30ca454d6201bb171901b9b80f1
jst1to5y <dev.tokhy@gmail.com>
2023-05-01 02:07:37 +0300

    Add Another solution

--- level-1/1.py
+++ level-1/1.py
@@ -4,6 +4,8 @@ the odd and even numbers in the x,y range
 """
 odd = []
 even = []
+
+
 def make_lists(x, y):
     for num in range(x, y + 1):
         if num % 2 != 0:
@@ -12,4 +14,18 @@ def make_lists(x, y):
             even.append(num)
     return f'Odd: {odd}\nEven: {even}'
 
+
 print(make_lists(1, 10))
+
+"""
+#### Solution 2 ####
+"""
+
+
+def make_lists(x, y):
+    odd = [num for num in range(x, y + 1) if num % 2 != 0]
+    even = [num for num in range(x, y + 1) if num % 2 == 0]
+    return f'Odd: {odd}\nEven: {even}'
+
+
+print(make_lists(10, 20))
