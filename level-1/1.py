7e6199f83176cb37b0f5fe697a57dfc8ecc9b3cb
jst1to5y <dev.tokhy@gmail.com>
2023-05-01 01:43:16 +0300

    Solution 1

---
+++ level-1/1.py
@@ -0,0 +1,15 @@
+"""
+Create a python function that takes 2 numbers x,y and prints 2 lists containing
+the odd and even numbers in the x,y range
+"""
+odd = []
+even = []
+def make_lists(x, y):
+    for num in range(x, y + 1):
+        if num % 2 != 0:
+            odd.append(num)
+        else:
+            even.append(num)
+    return f'Odd: {odd}\nEven: {even}'
+
+print(make_lists(1, 10))
