b6884fa53412e52d62d6214c5cb9ae196baa126d
jst1to5y <dev.tokhy@gmail.com>
2023-05-01 16:48:42 +0300

    Task 2

---
+++ level-1/2.py
@@ -0,0 +1,21 @@
+"""
+Create a python function that takes 2 numbers x,y and prints
+all the numbers between 1 and 100 than can be divided on x,y
+"""
+
+nums = []
+def divided_on(x, y):
+    for num in range(1, 101):
+        if (num % x == 0) and (num % y == 0):
+            nums.append(num)
+    print(nums)
+
+
+divided_on(3, 5)
+
+
+
+
+
+
+
