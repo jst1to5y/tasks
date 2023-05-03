69feb51cb6f7faf3d170714baceca356f22e053d
jst1to5y <dev.tokhy@gmail.com>
2023-05-03 20:43:09 +0300

    Task 8 - Solution 1

---
+++ level-1/8.py
@@ -0,0 +1,15 @@
+"""
+Create a function that takes x,y and prints all the number that can be divided by y from x to 100
+"""
+
+# Solution 1
+def divided_by_y(x, y):
+    nums = []
+    for num in range(x, 101):
+        if num % y == 0:
+            nums.append(num)
+    print(nums)
+
+
+divided_by_y(50, 3)
+
