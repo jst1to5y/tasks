cd851693cd5f0f4eaefdb687a93b98a0f2a6bb42
jst1to5y <dev.tokhy@gmail.com>
2023-05-04 05:23:43 +0300

    Add level 2 - Task 1

---
+++ level-2/1.py
@@ -0,0 +1,22 @@
+"""
+1. Transform all names to uppercase using [normal list - list comprehension - functional programming]
+"""
+names = ["mahmoud", "farida", "ali", "hassan", "mohamed", "khaled", "taha"]
+# Uppercase Normal list
+upper_names = []
+for name in names:
+    upper_names.append(name.upper())
+
+print(f"Normal list: {upper_names}")
+
+# Uppercase list comprehension
+upper_names = [name.upper() for name in names]
+print(f"List comprehension: {upper_names}")
+
+# Uppercase functional programming
+def upperCase(x):
+    return x.upper()
+
+
+upper_names = list(map(upperCase, names))
+print(f"Functional programming: {upper_names}")
