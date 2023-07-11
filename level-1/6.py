f47040d95aeca2effd6cf84c3cbb3caf878ed208
jst1to5y <dev.tokhy@gmail.com>
2023-07-11 16:28:28 +0300

    Edit task-6

--- level-1/6.py
+++ level-1/6.py
@@ -3,11 +3,13 @@ Create a function that takes a sentence and word and remove the word from the se
 """
 
 def remove_word(phrase, word):
-    list_phrase = phrase.split(" ")
-    if word in list_phrase:
-        list_phrase.remove(word)
-    print(" ".join(list_phrase))
+    words = phrase.split(" ")
+    new_sentance = []
+    for w in words:
+        if w != word:
+            new_sentance.append(w) 
+    print(" ".join(new_sentance))
 
 
-remove_word("hello hi there", "hi")
+remove_word("hi hello hi there hi ", "hi")
 
